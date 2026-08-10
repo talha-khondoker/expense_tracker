58c6354262bac0ddc21794a8a1d271451d326d25
Talha Khondoker <khondokertalha@gmail.com>
2026-08-10 23:01:03 +0600

    changed database url

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/ExpenseTrackerDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.kylfmdpdqoeyvgkvzjda:khondoker11@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,echo=True)
 
